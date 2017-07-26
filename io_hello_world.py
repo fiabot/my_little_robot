2f37f75c171ba5f0de3f3fba85bf44cb222817fa
fiabot <fiona.shyne@gmail.com>
2017-07-26 18:44:18 -0500

    Update io_hello_world.py

--- io_hello_world.py
+++ io_hello_world.py
@@ -12,13 +12,13 @@ ADAFRUIT_IO_KEY = "07939487d2614d2482d79902f43486a9"
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_KEY)
 
-on_data = aio.receive('on_feed') 
+on_data.value = aio.receive('on_feed') 
 if on_data == "ON": 
   while true:
     data = aio.receive('speed_feed')
     print('Latest value from speed_feed: {0}'.format(data.value))
     on_data = aio.receive('on_feed') 
-    if on_data == "OFF": 
+    if on_data.value == "OFF": 
       break
     time.sleep(1)
 
