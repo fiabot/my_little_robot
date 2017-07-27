e1888bb2c65242f654d07fd7f30134b09ffd21a5
fiabot <fiona.shyne@gmail.com>
2017-07-26 19:02:11 -0500

    Update io_robo_control.py

--- io_robo_control.py
+++ io_robo_control.py
@@ -4,3 +4,18 @@
 import time 
 from Adafruit_IO import Client
 import Robot
+
+# globals 
+LEFT_TRIM   = 0
+RIGHT_TRIM  = 0
+ADAFRUIT_IO_KEY = "07939487d2614d2482d79902f43486a9" #adafruit io key 
+aio = Client(ADAFRUIT_IO_KEY) #set up io client 
+robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM) #define the robot
+
+
+while True: 
+  
+  #break out of loop if the toggle button is off 
+  on_data = aio.receive("on_feed") 
+  if on_data.value == "OFF": 
+    break 
