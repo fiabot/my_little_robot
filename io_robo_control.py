4adfbce87c02cc94db7c7700abe2559c3d815e60
fiabot <fiona.shyne@gmail.com>
2017-07-26 21:22:54 -0500

    Update io_robo_control.py

--- io_robo_control.py
+++ io_robo_control.py
@@ -8,10 +8,43 @@ import Robot
 # globals 
 LEFT_TRIM   = 0
 RIGHT_TRIM  = 0
+ANGLE_MIN = -90 
+ANGLE_MAX = 90 
+THROTTLE_MAX = 255 
+THROTTLE_MIN = -255
 ADAFRUIT_IO_KEY = "07939487d2614d2482d79902f43486a9" #adafruit io key 
 aio = Client(ADAFRUIT_IO_KEY) #set up io client 
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM) #define the robot
 
+#map value from left range to right range
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+  
+def run_motor(angle,throttle,seconds = None): 
+  global ANGLE_MIN, ANGLE_MAX,THROTTLE_MIN,THROTTLE_MAX, robot
+  
+  if angle < 0: 
+    right_val = throttle 
+    left_val = translate(angle, ANGLE_MIN,ANGLE_MAX,THROTTLE_MIN,THROTTLE_MAX)
+  elif angle >0: 
+    left_val = throttle 
+    right_val = translate(angle,ANGLE_MIN,ANGLE_MAX,THROTTLE_MIN,THROTTLE_MAX) 
+  else: 
+    left_val = 0
+    right_val = 0 
+  
+  robot.move_gen(left_val,right_val)
+  
+   
 
 while True: 
   
@@ -19,3 +52,20 @@ while True:
   on_data = aio.receive("on_feed") 
   if on_data.value == "OFF": 
     break 
+  
+  #get angle from io
+  angle_data = aio.receive("turn_feed") 
+  
+  #get throttle data 
+  throttle_data = aio.receive("speed_feed") 
+  
+  #run robot 
+  run_motor(angle_data.value, throttle_data.value)
+  
+  time.sleep(0.1)
+  
+  
+  
+  
+  
+  
