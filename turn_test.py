500435b7d21a90ff06a7b33695d39735f27c5c7d
fiabot <fiona.shyne@gmail.com>
2017-08-07 14:50:59 -0500

    Update turn_test.py

--- turn_test.py
+++ turn_test.py
@@ -11,8 +11,12 @@ robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM) #define the robo
 print("fast turn") 
 robot.move_gen(-throttle,throttle,1) 
 
+time.sleep(1)
+
 print("medium turn") 
 robot.move_gen(0,throttle,1) 
+time.sleep(1)
 
 print("slow turn") 
 robot.move_gen(throttle/2,throttle,1) 
+time.sleep(1)
